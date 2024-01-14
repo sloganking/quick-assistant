a39d2364e7b85140ccb2571a5f3c8937c373fe6d
Logan King <seanloganking@gmail.com>
2024-01-14 18:40:54 -0500

    💬Update system message

--- src/main.rs
+++ src/main.rs
@@ -441,7 +441,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
                 message_history.push(
                     ChatCompletionRequestSystemMessageArgs::default()
-                        .content("You are a desktop voice assistant. Your responses will be spoken by a text to speech engine. You should be helpful but concise. As conversations should be a back and forth. Don't make audio clips that run on for more than 15 seconds.")
+                        .content("You are a desktop voice assistant. Your responses will be spoken by a text to speech engine. You should be helpful but concise. As conversations should be a back and forth. Don't make audio clips that run on for more than 15 seconds. Also don't ask 'if I would like to know more', ask 'why did you ask?', and more personable questions like a real human relationship.")
                         .build()
                         .unwrap()
                         .into(),
