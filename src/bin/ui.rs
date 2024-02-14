658e5ee1beaae2647f3b0cbab2c735fc1485e4ef
Logan King <seanloganking@gmail.com>
2024-02-13 20:03:58 -0800

    🐛[ui] disable send button if message empty

--- src/bin/ui.rs
+++ src/bin/ui.rs
@@ -11,8 +11,10 @@ slint::slint! {
         title: "quick-assistant";
 
         callback handle_message;
+        callback disable_send_if_empty_message;
         in-out property <string> message_history <=> message_history_text.text;
         in-out property <string> message <=> message_lineedit.text;
+        in property <bool> send_button_enabled <=> send_button.enabled;
 
         VerticalLayout {
             width: 66%;
@@ -55,14 +57,16 @@ slint::slint! {
                             handle_message();
                         }
                         // width: 80%;
+                        edited => {disable_send_if_empty_message()}
                     }
-                    Button {
+                    send_button := Button {
                         text: "Send";
                         // width: 50px;
                         primary: true;
                         clicked => {
                             handle_message();
                         }
+                        enabled: false;
                     }
                 }
             }
@@ -82,11 +86,24 @@ fn main() {
         let main_window: MainWindow = main_window_weak.unwrap();
 
         let message: String = main_window.get_message().into();
+
+        if message.trim().is_empty() {
+            return;
+        }
+
         let mut current_message_history: String = main_window.get_message_history().into();
         current_message_history.push_str(&format!("\n{}", message));
 
         main_window.set_message_history(current_message_history.into());
         main_window.set_message("".into());
+        main_window.set_send_button_enabled(false);
+    });
+
+    let main_window_weak = main_window.as_weak();
+    main_window.on_disable_send_if_empty_message(move || {
+        let main_window: MainWindow = main_window_weak.unwrap();
+        let message: String = main_window.get_message().into();
+        main_window.set_send_button_enabled(!message.trim().is_empty());
     });
 
     main_window.run().unwrap();
